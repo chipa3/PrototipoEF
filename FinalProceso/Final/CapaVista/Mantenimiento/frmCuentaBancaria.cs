dfad743e54dbb83561372431d05132cc9afc91fc
chipa3 <chipa_124578@outlook.com>
2020-11-24 10:46:37 -0600

    Transacciones
    Se termino el proceso de las transacciones

---
+++ FinalProceso/Final/CapaVista/Mantenimiento/frmCuentaBancaria.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista.Mantenimiento
+{
+    public partial class frmCuentaBancaria : Form
+    {
+        string UsuarioAplicacion;
+        static Form FormularioPadre;
+        Controlador Cn = new Controlador();
+        public frmCuentaBancaria(string usuario, Form formularioPadre)
+        {
+            InitializeComponent();
+            UsuarioAplicacion = usuario;
+            navegador1.Usuario = UsuarioAplicacion;
+            FormularioPadre = formularioPadre;
+        }
+
+        private void rbtnActivo_CheckedChanged(object sender, EventArgs e)
+        {
+            //si se selecciona el radioButon de inactivo, el dato que se reflejara en el campo de texto sera e estado  1
+            if (rbtnActivo.Checked == true)
+            {
+                txtEstado.Text = "1";
+            }
+        }
+
+        private void rbtnInactivo_CheckedChanged(object sender, EventArgs e)
+        {
+            //si se selecciona el radioButon de inactivo, el dato que se reflejara en el campo de texto sera e estado  0
+            if (rbtnInactivo.Checked == true)
+            {
+                txtEstado.Text = "0";
+            }
+        }
+
+        private void txtEstado_TextChanged(object sender, EventArgs e)
+        {
+            //si el campo estado esta vacio coloca los 2 radioButons en falso, para que se puedan volver a seleccionar
+            if (txtEstado.Text == "")
+            {
+                rbtnActivo.Checked = false;
+                rbtnInactivo.Checked = false;
+            }
+            if (txtEstado.Text == "1")
+            {
+                rbtnActivo.Checked = true;
+            }
+        }
+     
+        private void cmbBanco_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCodigoBanco.SelectedIndex = cmbBanco.SelectedIndex;
+        }
+
+        private void cmbCodigoBanco_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbBanco.SelectedIndex = cmbCodigoBanco.SelectedIndex;
+        }
+
+        private void cmbPropietario_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCodigoPropietario.SelectedIndex = cmbPropietario.SelectedIndex;
+        }
+
+        private void cmbCodigoPropietario_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbPropietario.SelectedIndex = cmbCodigoPropietario.SelectedIndex;
+        }
+
+        private void cmbMoneda_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCodigoMoneda.SelectedIndex = cmbMoneda.SelectedIndex;
+        }
+
+        private void cmbCodigoMoneda_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbMoneda.SelectedIndex = cmbCodigoMoneda.SelectedIndex;
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+            List<string> CamposTabla = new List<string>();
+            List<Control> lista = new List<Control>();
+            //el numero de aplicacion se debe cambiar por el numero asignado en la base de datos 
+            navegador1.aplicacion = 1305;
+            //banco se debe cambiar por la tabla a la que se quiere hacer el mantenimiento
+            navegador1.tbl = "cuenta_bancaria";
+            //estado_banco se debe cambiar por el estado de la tabla a la que se desea hacer mantenimiento
+            navegador1.campoEstado = "estado_apertura_cuenta_bancaria";
+            navegador1.MDIformulario = FormularioPadre;
+            foreach (Control C in this.Controls)
+            {
+                if ((C.Tag != null) && (!C.Tag.ToString().Equals("")))
+                {
+                    if (C is TextBox)
+                    {
+                        lista.Add(C);
+
+                    }
+                    else if (C is ComboBox)
+                    {
+                        lista.Add(C);
+
+                    }
+                    else if (C is DateTimePicker)
+                    {
+                        lista.Add(C);
+                    }
+                }
+            }
+            navegador1.control = lista;
+            navegador1.formulario = this;
+            //el dataGridView1 se debe cambiar por el que se tiene en el formulario
+            navegador1.DatosActualizar = dgvDatos;
+            navegador1.procActualizarData();
+            try
+            {
+                navegador1.procCargar();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            //en la variable ayuda ruta debe colocar la carpeta y el archivo.chm de su proyecto de ayuda
+            navegador1.ayudaRuta = "AyudaFRM/AyudaFRM.chm";
+            navegador1.ruta = "Cuenta.html";
+        }
+
+        private void frmCuentaBancaria_Load(object sender, EventArgs e)
+        {
+            procLlenarCmb("banco", "pk_id_banco", cmbCodigoBanco);
+            procLlenarCmb("banco", "nombre_banco", cmbBanco);
+            procLlenarCmb("propietario", "pk_id_propietario", cmbCodigoPropietario);
+            procLlenarCmb("propietario", "nombre_propietario", cmbPropietario);
+            procLlenarCmb("tipo_moneda", "pk_id_tipo_moneda", cmbCodigoMoneda);
+            procLlenarCmb("tipo_moneda", "moneda_tipo_moneda", cmbMoneda);
+        }
+
+        //funcion reutilizada del proyecto
+        public void procLlenarCmb(string Tabla, string Campo1, ComboBox CmbAgregar)
+        {
+            string[] Items = Cn.funcItems(Tabla, Campo1);
+            for (int I = 0; I < Items.Length; I++)
+            {
+                if (Items[I] != null)
+                {
+                    if (Items[I] != "")
+                    {
+                        CmbAgregar.Items.Add(Items[I]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FinalProceso/Final/CapaVista/Proceso/frmTransacciones.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista.Proceso
+{
+    public partial class frmTransacciones : Form
+    {
+        Controlador Cn = new Controlador();
+        public frmTransacciones()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTransacciones_Load(object sender, EventArgs e)
+        {
+            llenarCombos();
+            cargarDatos();
+        }
+        void llenarCombos()
+        {
+            //codigo reutilizado del proyecto
+            procLlenarCmb("cuenta_bancaria", "pk_id_numero_cuenta_bancaria", cmbCodigoCuenta);
+            procLlenarCmb("tipo_transaccion", "pk_id_tipo_transaccion", cmbCodigoTransaccion);
+            procLlenarCmb("tipo_transaccion", "nombre_tipo_transaccion", cmbTransaccion);
+            procLlenarCmb("tipo_moneda", "pk_id_tipo_moneda", cmbCodigoMoneda);
+            procLlenarCmb("tipo_moneda", "moneda_tipo_moneda", cmbMoneda);
+        }
+
+        public void procLlenarCmb(string Tabla, string Campo1, ComboBox CmbAgregar)
+        {
+            string[] Items = Cn.funcItems(Tabla, Campo1);
+            for (int I = 0; I < Items.Length; I++)
+            {
+                if (Items[I] != null)
+                {
+                    if (Items[I] != "")
+                    {
+                        CmbAgregar.Items.Add(Items[I]);
+                    }
+                }
+            }
+        }
+
+        private void cmbCodigoCuenta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtSaldoActual.Text = Cn.funcValor(Int32.Parse(cmbCodigoCuenta.SelectedItem.ToString())).ToString();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            float Resultado = 0;
+            if (txtDescripcion.Text == "" || txtMonto.Text == "" )
+            {
+                MessageBox.Show("No debe dejar campos vacios", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                int codigo = Cn.Codigo();
+                int cuenta = Int32.Parse(cmbCodigoCuenta.SelectedItem.ToString());
+                int moneda = Int32.Parse(cmbCodigoMoneda.SelectedItem.ToString());
+                int tipoTransaccion = Int32.Parse(cmbCodigoTransaccion.SelectedItem.ToString());
+                string fecha = dtpFecha.Value.ToString("yyyy-MM-dd HH:MM"); ;
+                float monto = float.Parse(txtMonto.Text);
+                string descripcion = txtDescripcion.Text;
+                float saldoAnterior = float.Parse(txtSaldoActual.Text);       
+                if(tipoTransaccion == 1)
+                {
+                    Resultado = monto + saldoAnterior;
+                    Cn.ParametrosIngresar(codigo, cuenta, fecha, tipoTransaccion, moneda, monto, descripcion);
+                    Cn.saldoNuevo(cuenta, Resultado);
+                    MessageBox.Show("Todos los datos fueron ingresados Correctamente", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Limpiar();
+                    llenarCombos();
+                    cargarDatos();
+                }
+                else if(tipoTransaccion == 2)
+                {
+                    if(monto > saldoAnterior)
+                    {
+                        MessageBox.Show("El monto que desea restar es mayor al que esta registrado", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        Resultado = saldoAnterior - monto;
+                        Cn.ParametrosIngresar(codigo, cuenta, fecha, tipoTransaccion, moneda, monto, descripcion);
+                        Cn.saldoNuevo(cuenta, Resultado);
+                        MessageBox.Show("Todos los datos fueron ingresados Correctamente", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Limpiar();
+                        llenarCombos();
+                        cargarDatos();
+                    }
+                }
+             
+            }
+        }
+
+        void cargarDatos()
+        {
+            DataTable dt = Cn.enviar();
+            dgvDatos.DataSource = dt;
+        }
+
+        private void cmbTransaccion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCodigoTransaccion.SelectedIndex = cmbTransaccion.SelectedIndex;
+        }
+
+        private void cmbMoneda_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbCodigoMoneda.SelectedIndex = cmbMoneda.SelectedIndex;
+        }
+
+        void Limpiar()
+        {
+            txtSaldoActual.Text = "";
+            txtDescripcion.Text = "";
+            txtMonto.Text = "";
+            cmbCodigoCuenta.Items.Clear();
+            cmbCodigoTransaccion.Items.Clear();
+            cmbTransaccion.Items.Clear();
+            cmbMoneda.Items.Clear();
+            cmbCodigoMoneda.Items.Clear();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            llenarCombos();
+        }
+    }
+}
